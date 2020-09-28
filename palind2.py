974618e1583f866110a7b6fcdbd16d6f21552af2
snguyen227 <70257943+snguyen227@users.noreply.github.com>
2020-09-28 15:28:40 -0700

    Add files via upload

---
+++ palind2.py
@@ -0,0 +1,21 @@
+def palind(s):
+    rev=""
+    for i in s:
+        rev=i+rev
+    if rev==s:
+        return True
+    return False
+print(palind("madammimadam"))
+print(palind("madamimadam"))
+print(palind("ratsliveonnoevilstar"))
+print(palind("amanaplanacanalpanama"))
+
+def palind2(s):
+ alist=list(s)
+ alist.reverse()
+ revs="".join(alist)
+ if revs==s:
+     return True
+ return False
+print(palind2("ratsliveonnoevilstar"))
+print(palind2("abcdedcba")) 
